6b7cdcfb4130085097ebb11c7f521ee7b510e3b4
Rifqy232 <rachmanmry20d@student.unhas.ac.id>
2023-03-27 23:41:54 +0800

    Add features: volume, surface area, and circumference

---
+++ app/src/main/java/com/example/unittestexercise/CuboidModel.kt
@@ -0,0 +1,25 @@
+package com.example.unittestexercise
+
+class CuboidModel {
+    private var width = 0.0
+    private var height = 0.0
+    private var length = 0.0
+
+    fun getVolume(): Double = width * height * length
+
+    fun getSurfaceArea(): Double {
+        val wl = width * length
+        val wh = width * height
+        val lh = height * length
+
+        return 2 * (wl * wh * lh)
+    }
+
+    fun getCircumference(): Double = 4 * (width + height + length)
+
+    fun save(width: Double, height: Double, length: Double) {
+        this.width = width
+        this.length = length
+        this.height = height
+    }
+}
